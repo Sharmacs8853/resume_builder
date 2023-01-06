2c3f28ff6877d2381927ca72e590e950ab5ec780
sharmacs8853 <sharmacs8853@gmail.com>
2023-01-06 14:06:06 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Box } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Box>
+      <h1>welcome to resume builder</h1>
+      
+    </Box>
+  );
+}
+
+export default App;
